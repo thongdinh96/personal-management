a8f7a32fed802ca86c922150408e696e103f9279
Thong <dt96dn@gmail.com>
2020-03-16 18:03:29 +0700

    list youtube

--- PersonalManagement/Keys.cs
+++ PersonalManagement/Keys.cs
@@ -8,7 +8,7 @@ namespace PersonalManagement
     public static class Keys
     {
         public static string SMSAccountIdentification = "AC4636675281b5ae9887ac864c27cd5ed8";
-        public static string SMSAccountPassword = "0ace9a2b1050deb64fad48a51fb2fc17";
+        public static string SMSAccountPassword = "256b21e07737b4fbafcf67a288735114";
         public static string SMSAccountFrom = "+14234370640";
         public static string OpenWeatherApiKey = "339eadb18d83b9a8fbd2ef12e749fb25";
     }
--- PersonalManagement/Scripts/custom-youtube.js
+++ PersonalManagement/Scripts/custom-youtube.js
@@ -11,7 +11,7 @@
             .then(function () { console.log("GAPI client loaded for API"); },
                 function (err) { console.error("Error loading GAPI client for API", err); });
     }
-    function execute() {
+     function execute() {
         return gapi.client.youtube.subscriptions.list({
             "part": "snippet,contentDetails",
             "maxResults": 50,
@@ -24,13 +24,14 @@
                 const colCount = 6;
                 var i = 1;
                 var divRow;
+                
                 $.each(subs, function (index, sub) {
 
                     if (i == 1) {
                         divRow = $("<div></div>");
                         divRow.attr("class", "row");
                     }
-                    var channelId = sub.id;
+                    var channelId = sub.snippet.resourceId.channelId;
                     var imgUrl = sub.snippet.thumbnails.medium.url;
                     var imgEle = $("<img>");
                     imgEle.attr("src", imgUrl);
@@ -38,25 +39,55 @@
                     imgEle.attr("height",240)
                     imgEle.attr("class", "card-img-top");
                     var divImg = $("<div></div>")
-                    divImg.attr("class", `card col-md-${12 / colCount}`)
+                    divImg.attr("class", `card col-md-${12 / colCount} mb-4`)
                     
                     divImg.append(imgEle)
                     var cardBody = $("<div></div")
                     cardBody.attr("class", "card-body")
                     var cardTitle = $("<h5></h5>")
                     cardTitle.attr("class", "card-title").text(sub.snippet.title)
+                    var desPara = $($.parseHTML("<p class='card-text'><b>Mô tả:</b></p>"));
+                    desPara.html(desPara.html() + ' ' + sub.snippet.description)
+                    
                     cardBody.append(cardTitle)
+                    executeChannel(channelId, cardBody, desPara)
+                    
                     divImg.append(cardBody)
                     divRow.append(divImg);
                     i++;
-                    if (i == colCount+1) {
+                    if (i == colCount + 1 || index==subs.length-1) {
                         $($(".container-fluid")[0]).append(divRow); i = 1;
                     }
                 });
             },
                 function (err) { console.error("Execute error", err); });
     }
+    function executeChannel(id, cardBody, desPara) {
+        console.log(id)
+        return gapi.client.youtube.channels.list({
+            "part": "snippet,contentDetails,statistics",
+            "id": id
+        })
+            .then(function (response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response channel", response);
+                var publishDate = $($.parseHTML("<p class='card-text'><b>Ngày lập kênh:</b></p>"));
+                var pd = new Date(response.result.items[0].snippet.publishedAt);
+                publishDate.html(publishDate.html() + ` ${pd.getDate()}/${pd.getMonth() + 1}/${pd.getFullYear()}`)
+                var totalSub = $($.parseHTML("<p class='card-text'><b>Lượt đăng ký:</b></p>"));
+                totalSub.html(totalSub.html() + ' ' + parseInt(response.result.items[0].statistics.subscriberCount).toLocaleString('vi-VN'))
+                var totalView = $($.parseHTML("<p class='card-text'><b>Lượt xem:</b></p>"));
+                totalView.html(totalView.html() + ' ' + parseInt(response.result.items[0].statistics.viewCount).toLocaleString('vi-VN'))
+                cardBody.append(totalSub)
+                cardBody.append(totalView)
+                cardBody.append(publishDate);
+                cardBody.append(desPara)
+
+
+            },
+                function (err) { console.error("Execute error", err); });
+    }
     gapi.load("client:auth2", function () {
-        gapi.auth2.init({ client_id: "755793331580-j80vi8bnrd2nivd0qobflrvubrsfhmre.apps.googleusercontent.com" }).then(authenticate).then(loadClient).then(execute);;
+        gapi.auth2.init({ client_id: "755793331580-j80vi8bnrd2nivd0qobflrvubrsfhmre.apps.googleusercontent.com" }).then(authenticate).then(loadClient).then(execute)
     });
 });
